f4b0468f2dd4778be7f8441502190f7d7da2e0ea
kevin_Arpe <arpeggiogio@gmail.com>
2022-01-25 20:38:36 +0900

    Update server.js for using http/ws lib

--- src/server.js
+++ src/server.js
@@ -1,10 +1,21 @@
 import express from "express";
+import http from "http";
+import WebSocket from "ws";
 
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
+
 const handleListen = () => console.log("listening on http://localhost:3000");
-app.listen(3000, handleListen);
+
+
+
+const server = http.createServer(app);
+// http, websocket 모두를 사용하도록 설정
+const wss = new WebSocket.Server({ server });
+
+server.listen(3000, handleListen);
