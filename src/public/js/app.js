a614dc05900dbe3fa57f0cdb6facaffc3b8b7545
kevin_Arpe <arpeggiogio@gmail.com>
2022-01-29 17:16:45 +0900

    코드 정리

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -2,22 +2,25 @@
 const messageList = document.querySelector("ul");
 const messageForm = document.querySelector("form");
 
-
+// WebSocket API로 객체 생성
 const socket = new WebSocket(`ws://${window.location.host}`);
 
+// 소켓 통신 Open
 socket.addEventListener("open", () => {
     console.log("Connected to server ✅");
 });
 
+// message를 받았을 때
 socket.addEventListener("message", (msg) => {
     console.log(`Me : "${msg.data}"`);
 });
 
+// 통신이 종료되었을 때
 socket.addEventListener("close", () => {
     console.log("Disconnected from server 🚫");
 });
 
-
+/* Functions */
 function handleSubmit(event) {
     event.preventDefault();
     const input = messageForm.querySelector("input");
@@ -26,4 +29,9 @@ function handleSubmit(event) {
     input.value = "";
 }
 
-messageForm.addEventListener("submit", handleSubmit);
+function init() {
+    messageForm.addEventListener("submit", handleSubmit);
+}
+
+/* Runtime Logic */
+init();
--- src/server.js
+++ src/server.js
@@ -1,38 +1,38 @@
-import express from "express";
-import http from "http";
-import WebSocket from "ws";
+import express from 'express';
+import http from 'http';
+import WebSocket from 'ws';
 
+/* Server Basic Code */
 const app = express();
 
-app.set("view engine", "pug");
-app.set("views", __dirname + "/views");
-app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (_, res) => res.render("home"));
-app.get("/*", (_, res) => res.redirect("/"));
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+app.use('/public', express.static(__dirname + '/public')); // image, video, html, css, javascript와 같은 정적 파일 경로 지정
+// app.use(express.static('public')); // 윗 줄은 가상의 경로를 사용하고 싶을때, 이 코드는 가상의 경로가 아닌 실제 경로를 사용할때..
+app.get('/', (_, res) => res.render('home'));
+app.get('/*', (_, res) => res.redirect('/')); // 사용자가 주소 뒤에 임의의 다른 주소를 이어 붙였을때 home으로 redirect
 
-const handleListen = () => console.log("listening on http://localhost:3000");
-
-
-
+// http, websocket 객체 생성
 const server = http.createServer(app);
-// http, websocket 모두를 사용하도록 설정
-const wss = new WebSocket.Server({ server });
+const wss = new WebSocket.Server({ server }); // http, websocket 모두를 사용하도록 설정
+
+// 각 접속자 별 socket을 배열에 담아 관리
 const sockets = [];
 
-wss.on("connection", (socket) => {
+wss.on('connection', (socket) => {
     sockets.push(socket);
-    console.log("Connected to browser ✅");
+    console.log('Connected to browser ✅');
 
-    socket.on("message", (msg) => {
+    socket.on('message', (msg) => {
         const message = msg.toString();
         console.log(`Browser : ${message}`);
         
         sockets.forEach((eachSocket) => eachSocket.send(message));
     })
 
-    socket.on("close", () => {
-        console.log("Disconnected from browser 🚫");
+    socket.on('close', () => {
+        console.log('Disconnected from browser 🚫');
     });
 });
 
-server.listen(3000, handleListen);
+server.listen(3000, () => console.log('listening on http://localhost:3000'));
